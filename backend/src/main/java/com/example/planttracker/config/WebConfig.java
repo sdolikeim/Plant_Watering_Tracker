f272fc04390cc8117216901557d7ffa2c6ae53d2
오현옥 <sdolikeim@gmail.com>
2026-06-10 17:10:53 +0900

    Add Kubernetes deployment practice

--- backend/src/main/java/com/example/planttracker/config/WebConfig.java
+++ backend/src/main/java/com/example/planttracker/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.planttracker.planttracker.config;
+package com.example.planttracker.config;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
